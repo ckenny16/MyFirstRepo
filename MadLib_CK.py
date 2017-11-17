98520e57991eb201a4d8f73953ab3396e7e6c169
ckenny16 <33756622+ckenny16@users.noreply.github.com>
2017-11-17 10:12:24 -0500

    First Upload

---
+++ MadLib_CK.py
@@ -0,0 +1,64 @@
+### Mad Libs ###
+### Source: http://ordinaryhumanlanguage.ca/blog/wp-content/uploads/2017/07/Mad-Libs-Example.jpg
+
+print ("Give me a noun")
+n1 = input()
+
+print ("Give me a person in the room")
+person1 = input()
+
+print ("Give me a verb")
+v1 = input()
+
+print ("Give me a part of the body")
+Part1 = input()
+
+print ("Give me an adjective")
+adj1 = input()
+
+print ("Give me a noun")
+n2 = input()
+
+print ("Give me a noun")
+n3 = input()
+
+print ("Give me a plural noun")
+pn1 = input()
+
+print ("Give me a type of liquid")
+liquid1 = input ()
+
+print ("Give me an adjective")
+adj2 = input()
+
+print ("Give me a noun")
+n4 = input()
+
+print ("Give me a noun")
+n5 = input()
+
+print ("Give me a noun")
+n6 = input()
+
+print ("Give me a plural noun")
+pn2 = input()
+
+print ("Give me a person in the room")
+person2 = input()
+
+print ("Give me a noun")
+n7 = input()
+
+print ("Give me a plural part of the body")
+part2 = input()
+
+### Begin Mad Lib Here ###
+
+print("It was Thanksgiving, and the scent of succulent roast " + n1 + " wafted through my house. ")
+print("'" + person1 + ", it's time to " + v1 + "!")
+print("My mother called ")
+print("I couldn't wait to get my " + part1 + "on that " +adj1 + "Thanksgiving meal. ")
+print("My family sat around the dining-room " + n2)
+
+      
+      
